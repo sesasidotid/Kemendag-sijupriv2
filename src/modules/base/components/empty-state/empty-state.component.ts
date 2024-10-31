29438c9ca1e6a62430fe5fa42ec555815019599e
Bayu Tri Nugroho <bayu@sesasi.id>
2024-10-31 18:00:22 +0700

    feat: add dynamic empty-state component

---
+++ src/modules/base/components/empty-state/empty-state.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginContext } from '../../commons/login-context';
+
+@Component({
+  selector: 'app-empty-state',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './empty-state.component.html',
+  styleUrl: './empty-state.component.scss'
+})
+export class EmptyStateComponent {
+  @Input() title: string = 'Tidak ada data';
+  @Input() description: string = 'Kamu bisa menambahkan data dengan menekan tombol dibawah ini.';
+  @Input() buttonText: string;
+  @Input() buttonAction: () => void = () => {};
+  @Input() buttonIcon: string;
+
+  constructor(private router: Router) {}
+
+  handleButtonClick() {
+    if (this.buttonAction) {
+      this.buttonAction();
+    }
+  }
+}
