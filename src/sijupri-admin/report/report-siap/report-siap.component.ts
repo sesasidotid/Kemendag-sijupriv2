3310c8bab5fbd7da39a5312c9f4090c1ce042190
Bayu Tri Nugroho <bayu@sesasi.id>
2024-12-03 18:07:35 +0700

    enhance(report-siap): add validation, loading state and etc

--- src/sijupri-admin/report/report-siap/report-siap.component.ts
+++ src/sijupri-admin/report/report-siap/report-siap.component.ts
@@ -12,6 +12,7 @@ import { ActionColumnBuilder, PagableBuilder, PageFilterBuilder, PrimaryColumnBu
 import { ReportGenerate } from '../../../modules/report/models/report-generate.model';
 import { PagableComponent } from '../../../modules/base/components/pagable/pagable.component';
 import { Instansi } from '../../../modules/maintenance/models/instansi.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-report-siap',
@@ -37,6 +38,8 @@ export class ReportSiapComponent {
   reportId: string = "siapReport";
 
   @ViewChild(PagableComponent) pagableComponent!: PagableComponent;
+
+  exportTypeLoading$ = new BehaviorSubject<boolean>(false);
 
   constructor(
     private confirmationService: ConfirmationService,
@@ -80,43 +83,10 @@ export class ReportSiapComponent {
   }
 
   ngOnInit() {
-    this.apiService.getData('/api/v1/instansi').subscribe({
-      next: ((response: any) => {
-        this.instansiList = response.map((instansi: { [key: string]: any; }) => new Instansi(instansi));
-      }),
-      error: ((error) => {
-        this.handlerService.handleException(error);
-      })
-    });
-
-    this.apiService.getData('/api/v1/unit_kerja').subscribe({
-      next: ((response: any) => {
-        this.unitKerjaList = response.map((unitKerja: { [key: string]: any; }) => new UnitKerja(unitKerja));
-      }),
-      error: ((error) => {
-        this.handlerService.handleException(error);
-      })
-    });
-
-    this.apiService.getData('/api/v1/provinsi').subscribe({
-      next: ((response: any) => {
-        this.provinsiList = response.map((provinsi: { [key: string]: any; }) => new Provinsi(provinsi));
-      }),
-      error: ((error) => {
-        this.handlerService.handleException(error);
-      })
-    });
-
-    this.apiService.getData('/api/v1/kab_kota').subscribe({
-      next: ((response: any) => {
-        this.kabKotaList = response.map((kabKota: { [key: string]: any; }) => new KabKota(kabKota));
-      }),
-      error: ((error) => {
-        this.handlerService.handleException(error);
-      })
-    });
+    this.getExportTypeData();
 
     this.addSiapReportForm = new FormGroup({
+      exportType: new FormControl(''),
       instansiId: new FormControl(''),
       instansiName: new FormControl(''),
       unitKerjaId: new FormControl(''),
@@ -129,64 +99,106 @@ export class ReportSiapComponent {
     })
   }
 
-  onInstansiChange(event: Event) {
-    const instansiId = (event.target as HTMLSelectElement).value;
-    for (const instansi of this.instansiList) {
-      if (instansi.id == instansiId) {
-        this.addSiapReportForm.get("instansiId").setValue(instansi.id)
-        this.addSiapReportForm.get("instansiName").setValue(instansi.name)
-        break;
-      } else {
-        this.addSiapReportForm.get("instansiId").setValue('')
-        this.addSiapReportForm.get("instansiName").setValue('')
-      }
-    }
+  getExportTypeData() {
+    this.exportTypeLoading$.next(true);
+    let loading: any = [false, false, false, false];
+
+    this.apiService.getData('/api/v1/instansi').subscribe({
+      next: ((response: any) => {
+        this.instansiList = response.map((instansi: { [key: string]: any; }) => new Instansi(instansi));
+      }),
+      complete: (() => {
+        loading[0] = true;
+        if(!loading.includes(false)) {
+          this.exportTypeLoading$.next(false);
+        }
+      }),
+      error: ((error) => {
+        this.handlerService.handleException(error);
+      })
+    });
+
+    this.apiService.getData('/api/v1/unit_kerja').subscribe({
+      next: ((response: any) => {
+        this.unitKerjaList = response.map((unitKerja: { [key: string]: any; }) => new UnitKerja(unitKerja));
+      }),
+      complete: (() => {
+        loading[1] = true;
+        if(!loading.includes(false)) {
+          this.exportTypeLoading$.next(false);
+        }
+      }),
+      error: ((error) => {
+        this.handlerService.handleException(error);
+      })
+    });
+
+    this.apiService.getData('/api/v1/provinsi').subscribe({
+      next: ((response: any) => {
+        this.provinsiList = response.map((provinsi: { [key: string]: any; }) => new Provinsi(provinsi));
+      }),
+      complete: (() => {
+        loading[2] = true;
+        if(!loading.includes(false)) {
+          this.exportTypeLoading$.next(false);
+        }
+      }),
+      error: ((error) => {
+        this.handlerService.handleException(error);
+      })
+    });
+
+    this.apiService.getData('/api/v1/kab_kota').subscribe({
+      next: ((response: any) => {
+        this.kabKotaList = response.map((kabKota: { [key: string]: any; }) => new KabKota(kabKota));
+      }),
+      complete: (() => {
+        loading[3] = true;
+        if(!loading.includes(false)) {
+          this.exportTypeLoading$.next(false);
+        }
+      }),
+      error: ((error) => {
+        this.handlerService.handleException(error);
+      })
+    });
   }
 
-  onUnitKerjaChange(event: Event) {
-    const unitKerjaId = (event.target as HTMLSelectElement).value;
-    for (const unitKerja of this.unitKerjaList) {
-      if (unitKerja.id == unitKerjaId) {
-        this.addSiapReportForm.get("unitKerjaId").setValue(unitKerja.id)
-        this.addSiapReportForm.get("unitKerjaName").setValue(unitKerja.name)
+  patchFormValue(event: Event, key: string) {
+    const id = (event.target as HTMLSelectElement).value;
+    switch (key) {
+      case "instansi":
+        this.instansiList.forEach((instansi) => {
+          if (instansi.id == id) {
+            this.addSiapReportForm.get("instansiName").setValue(instansi.name);
+          }
+        });
         break;
-      } else {
-        this.addSiapReportForm.get("unitKerjaId").setValue('')
-        this.addSiapReportForm.get("unitKerjaName").setValue('')
-      }
-    }
-  }
-
-  onProvinsiChange(event: Event) {
-    const provinsiId = (event.target as HTMLSelectElement).value;
-    for (const provinsi of this.provinsiList) {
-      if (provinsi.id == provinsiId) {
-        this.addSiapReportForm.get("provinsiId").setValue(provinsi.id)
-        this.addSiapReportForm.get("provinsiName").setValue(provinsi.name)
+      case "unitKerja":
+        this.unitKerjaList.forEach((unitKerja) => {
+          if (unitKerja.id == id) {
+            this.addSiapReportForm.get("unitKerjaName").setValue(unitKerja.name);
+          }
+        });
         break;
-      } else {
-        this.addSiapReportForm.get("provinsiId").setValue('')
-        this.addSiapReportForm.get("provinsiName").setValue('')
-      }
-    }
-  }
-
-  onKabKotaChange(event: Event) {
-    const kabKotaId = (event.target as HTMLSelectElement).value;
-    for (const kabKota of this.kabKotaList) {
-      if (kabKota.id == kabKotaId) {
-        this.addSiapReportForm.get("kabKotaId").setValue(kabKota.id)
-        this.addSiapReportForm.get("kabKotaName").setValue(kabKota.name)
+      case "provinsi":
+        this.provinsiList.forEach((provinsi) => {
+          if (provinsi.id == id) {
+            this.addSiapReportForm.get("provinsiName").setValue(provinsi.name);
+          }
+        });
         break;
-      } else {
-        this.addSiapReportForm.get("kabKotaId").setValue('')
-        this.addSiapReportForm.get("kabKotaName").setValue('')
-      }
+      case "kabKota":
+        this.kabKotaList.forEach((kabKota) => {
+          if (kabKota.id == id) {
+            this.addSiapReportForm.get("kabKotaName").setValue(kabKota.name);
+          }
+        });
+        break;
     }
   }
 
   onSubmit() {
-    this.addSiapReportForm.markAllAsTouched();
     if (this.addSiapReportForm.valid) {
       this.confirmationService.open(false).subscribe({
         next: (result) => {
@@ -195,31 +207,21 @@ export class ReportSiapComponent {
           const reportGenerate = new ReportGenerate();
           reportGenerate.reportId = this.reportId;
           reportGenerate.fileType = this.addSiapReportForm.value.fileType;
-          reportGenerate.parameter = {}
-          if (this.addSiapReportForm.value.instansiId != '') {
-            reportGenerate.parameter.instansiId = this.addSiapReportForm.value.instansiId;
-            reportGenerate.parameter.instansiName = this.addSiapReportForm.value.instansiName;
+          if (this.addSiapReportForm.value.exportType == '') {
+            reportGenerate.parameter = {}
           }
-          if (this.addSiapReportForm.value.unitKerjaId != '') {
-            reportGenerate.parameter.unitKerjaId = this.addSiapReportForm.value.unitKerjaId;
-            reportGenerate.parameter.unitKerjaName = this.addSiapReportForm.value.unitKerjaName;
+          if (this.addSiapReportForm.value.exportType == 'instansi') {
+            reportGenerate.parameter = { instansiId: this.addSiapReportForm.value.instansiId, instansiName: this.addSiapReportForm.value.instansiName }
           }
-          if (this.addSiapReportForm.value.provinsiId != '') {
-            reportGenerate.parameter.provinsiId = this.addSiapReportForm.value.provinsiId;
-            reportGenerate.parameter.provinsiName = this.addSiapReportForm.value.provinsiName;
+          if (this.addSiapReportForm.value.exportType == 'unitKerja') {
+            reportGenerate.parameter = { unitKerjaId: this.addSiapReportForm.value.unitKerjaId, unitKerjaName: this.addSiapReportForm.value.unitKerjaName }
           }
-          if (this.addSiapReportForm.value.kabKotaId != '') {
-            reportGenerate.parameter.kabKotaId = this.addSiapReportForm.value.kabKotaId;
-            reportGenerate.parameter.kabKotaName = this.addSiapReportForm.value.kabKotaName;
+          if (this.addSiapReportForm.value.exportType == 'provinsi') {
+            reportGenerate.parameter = { provinsiId: this.addSiapReportForm.value.provinsiId, provinsiName: this.addSiapReportForm.value.provinsiName }
           }
-
-          this.apiService.postData('/api/v1/report_generate/async', reportGenerate).subscribe({
-            next: () => {
-              this.handlerService.handleAlert("Success", "Report Generating")
-              this.pagableComponent.fetchData();
-              this.ngOnInit();
-            }
-          })
+          if (this.addSiapReportForm.value.exportType == 'kabKota') {
+            reportGenerate.parameter = { kabKotaId: this.addSiapReportForm.value.kabKotaId, kabKotaName: this.addSiapReportForm.value.kabKotaName }
+          }
         }
       })
     }
