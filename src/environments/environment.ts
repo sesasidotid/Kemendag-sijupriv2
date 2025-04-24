5c5449560b71eeb935be00845aa17e9c665df274
feisalsesasi <feisal@sesasi.id>
2025-04-24 09:31:21 +0700

    fix bug ternitary operation for isMenmguang in ukom

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,21 @@
 export const environment = {
-    production: true,
-    // appBaseHref: '/sijupri',
-    appBaseHref: '',
-    apiBaseUrl: 'http://sijupri.com:8000',
-    // apiBaseUrl: 'https://pusbinjfdag.id:8000',
-    firebaseConfig: {
-        apiKey: 'AIzaSyBHvB-ce9lpJdWbDJCBP2XLVJHYzV62oLw',
-        authDomain: 'sijupri.firebaseapp.com',
-        projectId: 'sijupri',
-        storageBucket: 'sijupri.firebasestorage.app',
-        messagingSenderId: '152999394823',
-        appId: '1:152999394823:web:7fe12972842c413c2e34a2',
-        measurementId: 'G-87JRW9M02W'
-    },
-    vapidKey:
-        'BFORBssxQyuPBlwobPpOY0L6MI-9RZM5ptuleUtGn4xkt2dNPOtP82zo45EtPOroRuxARpcLzjtm5RbrzNelto4',
-    recaptcha: {
-        siteKey: '6LfF3NUqAAAAAJrrXEvzsZi5vaa1oU6xFXvOj-uI'
-    }
+  production: true,
+  //   appBaseHref: '/sijupri',
+  appBaseHref: '',
+  apiBaseUrl: 'http://sijupri.com:8000',
+  //   apiBaseUrl: 'https://pusbinjfdag.id:8000',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBHvB-ce9lpJdWbDJCBP2XLVJHYzV62oLw',
+    authDomain: 'sijupri.firebaseapp.com',
+    projectId: 'sijupri',
+    storageBucket: 'sijupri.firebasestorage.app',
+    messagingSenderId: '152999394823',
+    appId: '1:152999394823:web:7fe12972842c413c2e34a2',
+    measurementId: 'G-87JRW9M02W'
+  },
+  vapidKey:
+    'BFORBssxQyuPBlwobPpOY0L6MI-9RZM5ptuleUtGn4xkt2dNPOtP82zo45EtPOroRuxARpcLzjtm5RbrzNelto4',
+  recaptcha: {
+    siteKey: '6LfF3NUqAAAAAJrrXEvzsZi5vaa1oU6xFXvOj-uI'
+  }
 }
