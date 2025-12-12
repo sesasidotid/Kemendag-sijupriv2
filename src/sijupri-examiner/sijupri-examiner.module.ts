38bd71c7e76fbde5a13a4625173fc16314b8089a
feisalsesasi <feisal@sesasi.id>
2025-12-12 11:27:46 +0700

    fix syntac error

--- src/sijupri-examiner/sijupri-examiner.module.ts
+++ src/sijupri-examiner/sijupri-examiner.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import {routes} from './sijupri-examiner.routes'
+import { routes } from './sijupri-examiner.routes'
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-      RouterModule.forChild(routes)
-  ]
+    declarations: [],
+    imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class SijupriExaminerModule { }
+export class SijupriExaminerModule {}
